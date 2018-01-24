e4559a3c891e62d2aafe3508af67a7771e121f25
Ben Ceglowski <ben.ceglowski@onefinestay.com>
2018-01-24 11:22:43 +0000

    Router works.

--- src/components/Switcher/index.jsx
+++ src/components/Switcher/index.jsx
@@ -1,29 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import universal from 'react-universal-component';
 
 import styles from '../../css/Switcher.css';
 
-const UniversalComponent = universal(({ page }) => import(`./${page}`), {
+const UniversalComponent = universal(({ page }) => import(`../${page}`), {
   minDelay: 500,
-
   loading: () => (
     <div className={styles.spinner}>
       <div />
     </div>
   ),
-
   error: () => <div className={styles.notFound}>PAGE NOT FOUND - 404</div>,
 });
 
-const Switcher = ({ page }) => (
+const component = ({ page }) => (
   <div className={styles.switcher}>
     <UniversalComponent page={page} />
   </div>
 );
 
-const mapStateToProps = state => ({
-  page: state.page,
-});
+component.propTypes = {
+  page: PropTypes.string.isRequired,
+};
 
-export default connect(mapStateToProps)(Switcher);
+export const mapStateToProps = ({ page }) => ({ page });
+
+export { component };
+
+export default connect(mapStateToProps)(component);
