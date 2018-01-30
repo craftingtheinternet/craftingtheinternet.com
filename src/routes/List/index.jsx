ecc5acede1d778a8f26527cb08b45b5b19e2f032
Ben Ceglowski <bceglowski@gmail.com>
2018-01-30 18:32:51 +0000

    Hey cool, self-portrait!

--- src/routes/List/index.jsx
+++ src/routes/List/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import ArticlePromotion from 'components/ArticlePromotion';
+import SelfPortrait from 'components/SelfPortrait';
 
 import styles from 'routes/List/styles.styl';
 
@@ -11,23 +11,11 @@ const component = ({
 }) => (
   <div className={styles.list}>
     <div className={styles.title}>Category: {category}</div>
-
     <div className={styles.content}>
       <ul>{packages.map(pkg => <li key={pkg}>{pkg}</li>)}</ul>
-
-      {category === 'redux' ? (
-        <ArticlePromotion
-          title="Wanna master data-fetching? Read:"
-          text="Redux-First Router data-fetching: solving the 80% use case for async Middleware 🚀"
-          url="https://medium.com/faceyspacey/redux-first-router-data-fetching-solving-the-80-use-case-for-async-middleware-14529606c262"
-        />
-      ) : (
-        <ArticlePromotion
-          title="New to Rudy?? Learn how it started and its motivation:"
-          text="Pre Release: Redux-First Router — A Step Beyond Redux-Little-Router 🚀"
-          url="https://medium.com/faceyspacey/pre-release-redux-first-router-a-step-beyond-redux-little-router-cd2716576aea"
-        />
-      )}
+      <div style={{ width: 460, height: 720 }}>
+        <SelfPortrait width={460} />
+      </div>
     </div>
   </div>
 );
