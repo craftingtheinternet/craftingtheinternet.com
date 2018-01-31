5089564cc2201730f8afcc797764434fc3accc31
Ben Ceglowski <bceglowski@gmail.com>
2018-01-31 09:24:49 +0000

    Abstracts CSS and SVG paths.

--- src/components/SelfPortrait/index.jsx
+++ src/components/SelfPortrait/index.jsx
@@ -1,5 +1,13 @@
 import universal from 'react-universal-component';
 
-const component = (typeof window === 'undefined' ? () => null : universal(() => import('./svg')));
+const noopComponent = () => null;
+const component = typeof window === 'undefined' ? (
+  noopComponent
+) : (
+  universal(() => import('./svg'), {
+    loading: noopComponent,
+    error: noopComponent,
+  })
+);
 
 export default component;
