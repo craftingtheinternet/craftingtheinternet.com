1e3f4108c84efe0cae4bf1f01343a5be02e62cf9
Ben Ceglowski <bceglowski@gmail.com>
2018-02-01 23:31:11 +0000

    Adds mah name.

--- src/reducers/title.js
+++ src/reducers/title.js
@@ -1,7 +1,7 @@
 export default (state = 'HOME', action = {}) => {
   switch (action.type) {
     case 'HOME':
-      return 'Crafting the Internet';
+      return 'Crafting the Internet — Ben Ceglowski';
     case 'ABOUT':
       return 'About — Crafting the Internet';
     default:
